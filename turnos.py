c07a339526a1a1c630a434b8a20e34d2a27a58ab
DAVID <johandavidcarvajalzapata94@gmail.com>
2026-03-06 10:49:57 -0500

    ARREGLAR QUE EL USUARIO SOLO PUEDA AGREGAR  1 Y 2 EN DISCAPACIDAD

--- turnos.py
+++ turnos.py
@@ -2,26 +2,31 @@
 contadorA = 0
 contadorB = 0
 contadorC = 0
+
 while True:
     # Solicitar datos al cliente
     try:
         edad = int(input("Ingrese su edad: "))
     except ValueError:
-        print("debe ingresar un numero, vuelva a intentarlo")
+        print("Debe ingresar un numero, vuelva a intentarlo")
         continue
-    print("¿Tiene alguna discapacidad?")  
+
+    print("¿Tiene alguna discapacidad?")
     print("1. Sí")
     print("2. No")
-    try:    
+
+    try:
         discapacidad = int(input("Seleccione una opción: "))
     except ValueError:
-        print("debe ingresar un numero para marcar su opcion, vuelva a intentarlo:")
-    
-        if discapacidad not in [1, 2]:
-            print("Opción inválida. Solo puede elegir 1 o 2.")
-        continue    
+        print("Debe ingresar un numero para marcar su opcion")
+        continue
 
-# Evaluar condiciones
+    # Validar que solo sea 1 o 2
+    if discapacidad not in [1, 2]:
+        print("Opción inválida. Solo puede elegir 1 o 2.")
+        continue
+
+    # Evaluar condiciones
     if discapacidad == 1:
         contadorC += 1
         turno = "C" + str(contadorC)
@@ -37,8 +42,8 @@ while True:
         turno = "A" + str(contadorA)
         tipo = "Turno normal"
 
-# Mostrar resultado
+    # Mostrar resultado
     print("\n===== TICKET =====")
-    print("Turno asignado:", turno) 
+    print("Turno asignado:", turno)
     print("Tipo de turno:", tipo)
     print("==================")
