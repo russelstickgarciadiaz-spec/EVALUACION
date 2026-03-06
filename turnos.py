ed6f2151a596c3cc4f9ff8a46c8ae7c6cc703b75
DAVID <johandavidcarvajalzapata94@gmail.com>
2026-03-06 10:06:01 -0500

    bucle para que se siga ejecutando

--- turnos.py
+++ turnos.py
@@ -2,33 +2,33 @@
 contadorA = 0
 contadorB = 0
 contadorC = 0
+while True:
+    # Solicitar datos al cliente
+    edad = int(input("Ingrese su edad: "))
 
-# Solicitar datos al cliente
-edad = int(input("Ingrese su edad: "))
-
-print("¿Tiene alguna discapacidad?")
-print("1. Sí")
-print("2. No")
-discapacidad = int(input("Seleccione una opción: "))
+    print("¿Tiene alguna discapacidad?")
+    print("1. Sí")
+    print("2. No")
+    discapacidad = int(input("Seleccione una opción: "))
 
 # Evaluar condiciones
-if discapacidad == 1:
-    contadorC += 1
-    turno = "C" + str(contadorC)
-    tipo = "Prioridad especial (Discapacidad)"
+    if discapacidad == 1:
+        contadorC += 1
+        turno = "C" + str(contadorC)
+        tipo = "Prioridad especial (Discapacidad)"
 
-elif edad >= 60:
-    contadorB += 1
-    turno = "B" + str(contadorB)
-    tipo = "Prioritario (Adulto mayor)"
+    elif edad >= 60:
+        contadorB += 1
+        turno = "B" + str(contadorB)
+        tipo = "Prioritario (Adulto mayor)"
 
-else:
-    contadorA += 1
-    turno = "A" + str(contadorA)
-    tipo = "Turno normal"
+    else:
+        contadorA += 1
+        turno = "A" + str(contadorA)
+        tipo = "Turno normal"
 
 # Mostrar resultado
-print("\n===== TICKET =====")
-print("Turno asignado:", turno)
-print("Tipo de turno:", tipo)
-print("==================")
+    print("\n===== TICKET =====")
+    print("Turno asignado:", turno) 
+    print("Tipo de turno:", tipo)
+    print("==================")
