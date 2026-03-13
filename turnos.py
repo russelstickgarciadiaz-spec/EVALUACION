eba3ed48fe775ec0ce4ac53fa7dd5aee4ea8c9d3
DAVID <johandavidcarvajalzapata94@gmail.com>
2026-03-13 07:19:47 -0500

    fin.

--- turnos.py
+++ turnos.py
@@ -4,29 +4,22 @@ contadorB = 0
 contadorC = 0
 
 while True:
-    # Solicitar edad
-    edad_input = input("Ingrese su edad: ")
-
-    # Validar que solo contenga números y no tenga espacios
-    if not edad_input.isdigit() or " " in edad_input:
-        print("Debe ingresar solo números sin espacios.")
+    # Solicitar datos al cliente
+    try:
+        edad = int(input("Ingrese su edad: "))
+    except ValueError:
+        print("Debe ingresar un numero, vuelva a intentarlo")
         continue
-
-    edad = int(edad_input)
 
     print("¿Tiene alguna discapacidad?")
     print("1. Sí")
     print("2. No")
 
-    # Solicitar discapacidad
-    discapacidad_input = input("Seleccione una opción: ")
-
-    # Validar que solo contenga números y no tenga espacios
-    if not discapacidad_input.isdigit() or " " in discapacidad_input:
-        print("Debe ingresar solo números sin espacios (1 o 2).")
+    try:
+        discapacidad = int(input("Seleccione una opción: "))
+    except ValueError:
+        print("Debe ingresar un numero para marcar su opcion")
         continue
-
-    discapacidad = int(discapacidad_input)
 
     # Validar que solo sea 1 o 2
     if discapacidad not in [1, 2]:
